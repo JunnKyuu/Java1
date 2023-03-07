14472c058b5014f5a850175a14ae68632a084487
JunnKyuu <junnkyuu22@gmail.com>
2023-03-07 22:07:30 +0900

    Hello World, for 문 연습

--- 230306_hello_sum/sum.java
+++ 230306_hello_sum/sum.java
@@ -1,14 +1,17 @@
-package test;
+package sum;
 
 public class sum {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
 		int sum = 0;
 		
-		for(int i = 1; i<11; i++) {
+		for(int i = 1; i< 11; i ++) {
 			sum += i;
 		}
 		
-		System.out.println(sum);
+		System.out.print(sum);
 	}
+
 }
