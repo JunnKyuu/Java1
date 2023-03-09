fe2b35f61a6aca352b964a4d2d41d8bb85b46c27
JunnKyuu <junnkyuu22@gmail.com>
2023-03-09 17:27:39 +0900

    while문을 이용한 echo

---
+++ 230308_echo/echo.java
@@ -0,0 +1,27 @@
+package echo;
+
+import java.util.Scanner;
+
+public class echo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str; // 입력받을 문자
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("x를 입력하면 종료가 됩니다.");
+		
+		while(true) {
+			str = scan.nextLine();
+			
+			if(str.equals("x")) {
+				break;
+			} else {
+				System.out.println(str);
+			}
+		}
+		
+		System.out.println("x를 입력하여 종료합니다.");
+	}
+}
