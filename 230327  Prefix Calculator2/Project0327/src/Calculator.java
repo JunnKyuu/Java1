0d96fed3fc160cc7130323e4cb6cfa9832a036a6
JunnKyuu <junnkyuu22@gmail.com>
2023-03-27 16:47:55 +0900

    Prefix Calulator V2 - prefix notation 구현

--- 230327  Prefix Calculator2/Project0327/src/Calculator.java
+++ 230327  Prefix Calculator2/Project0327/src/Calculator.java
@@ -1,12 +1,13 @@
 import java.io.IOException;
 
 public class Calculator {
-	private int readOperaor() throws IOException {
+	private int readOperator() throws IOException {
 		// TODO Auto-generated method stub
+		// 연산자를 판단하는 함수 
+		int operator = System.in.read();
+		System.in.read(); // LF가 들어가서 날려버린다. 
 		
-		int code = System.in.read();
-		System.in.read(); // linefeed가 들어감 근데 이것을 flush로 날려버려야한다! 
-		return code;
+		return operator;
 	}
 	
 	private int readInt() throws IOException {
@@ -14,51 +15,75 @@ public class Calculator {
 		
 		int number = 0;
 		int code = System.in.read();
+		
 		while(code >= '0' && code <= '9') {
 			number = number * 10 + (code - '0');
 			code = System.in.read();
-		} // 숫자가 아닌 것을 걸러냄 , CR은 괜찮다 
+		} // 숫자가 아닌 것을 걸러냄 
 		
 		if(code == LineFeed) {
-			code = System.in.read();
-			// 정상 
+			// 정상일 때 
 			return number;
 		} else {
-			// exception 발생, 입력을 잘못했으니 익셉션을 던져버림 
-			throw new IOException();
-			// 그냥 오류를 날리니까 리턴할 것이 없다  
+			// exception 발생
+			// 그냥 오류를 날리니까 리턴할 것이 없다
+			throw new IOException();  
 		}
 	} 
 	
 	private int add() throws IOException {
+		// 덧셈 함수 
 		int number1 = readInt(); // 이것도 맘에 안들어서 readOperand도 만들자! 
 		int number2 = readInt();
-		int sum = number1 + number2;
-		return sum;
+		int value = number1 + number2;
+		return value;
 	}
 	
 	private int multiply() throws IOException {
+		// 곱셈 함수 
 		int number1 = readInt();
 		int number2 = readInt();
-		int sum = number1 * number2;
-		return sum;
+		int value = number1 * number2;
+		return value;
+	}
+	
+	private int subtract() throws IOException {
+		// 뺄셈 함수 
+		int number1 = readInt();
+		int number2 = readInt();
+		int value = number1 - number2;
+		return value;
+	}
+	
+	private int divide() throws IOException {
+		// 나눗셈 함수 
+		int number1 = readInt();
+		int number2 = readInt();
+		int value = number1 / number2;
+		return value;
 	}
 	
 	public void run() {
 		try {
 			int result = 0;
-			int code = readOperaor();
+			int code = readOperator();
 			// 연산자를 읽어야 계산하니까 연산자를 읽는 함수를 만든다. 
 			
-			if(code == '*') {
+			if(code == '+') {
+				result = add();
+			} else if(code == '-') {
+				result = subtract();
+			} else if(code == '*') {
 				result = multiply();
+			} else {
+				result = divide();
 			}
-			
+			System.out.print("결과 : ");
 			System.out.println(result);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-//			System.exit(0);
+			System.exit(0);
 		}
 	}
 }
--- 230327  Prefix Calculator2/Project0327/src/Main.java
+++ 230327  Prefix Calculator2/Project0327/src/Main.java
@@ -12,6 +12,7 @@ public class Main {
 	}
 	
 	public static void main(String[] args) {
+		System.out.println("연산자(+, -, *, /)와 숫자 두 개를 순서대로 엔터를 이용해 입력하세요!");
 		Main main = new Main();
 		main.run();
 	}
