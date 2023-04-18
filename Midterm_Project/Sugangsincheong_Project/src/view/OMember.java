913b5bd670e1de76ded0efa75a05f4db145de52e
JunnKyuu <junnkyuu22@gmail.com>
2023-04-18 13:45:57 +0900

    Midterm_Project - Sugangsincheong_Project V1

---
+++ Midterm_Project/Sugangsincheong_Project/src/view/OMember.java
@@ -0,0 +1,15 @@
+package view;
+
+public class OMember {
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Midterm_Project/Sugangsincheong_Project/src/view/VLecture.java
@@ -0,0 +1,16 @@
+package view;
+
+import java.util.Scanner;
+
+public class VLecture {
+
+	public VLecture(Scanner scanner) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public OLecture show(String lectureFileName, String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
